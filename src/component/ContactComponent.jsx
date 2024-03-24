6c1267a3bace4806af4361ff2ba9091c9a4708a0
Ajay <ajaywankhade666@gmail.com>
2024-03-24 10:15:33 +0530

    contact details got changed

--- src/component/ContactComponent.jsx
+++ src/component/ContactComponent.jsx
@@ -1,5 +1,25 @@
+import React from "react";
+
 function ContactComponent() {
-  return <div>This is Contact Page</div>;
+  // Static contact details
+  const name = "Nike Store Customer Care";
+  const phoneNumber = "1-800-NIKE-123";
+  const email = "support@nike.com";
+  const address = "123 Nike Ave, Swoosh City";
+
+  return (
+    <div className="contact-container">
+      {" "}
+      {/* Apply container class */}
+      <h2>Contact Page</h2>
+      <p>This is the contact page.</p>
+      <p>Contact Details:</p>
+      <p>Name: {name}</p>
+      <p>Phone Number: {phoneNumber}</p>
+      <p>Email: {email}</p>
+      <p>Address: {address}</p>
+    </div>
+  );
 }
 
 export default ContactComponent;
